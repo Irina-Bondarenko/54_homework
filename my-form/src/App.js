62a5f5a298c092d97c412300f16a9c65c9412e9c
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-10-12 00:30:43 +0300

    init

---
+++ my-form/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import MyForm from "./components/MyForm";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container p-3">
+        <MyForm />
+      </div>
+    </div>
+  );
+}
+
+export default App;
